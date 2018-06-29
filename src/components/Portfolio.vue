29505fae05b3d69ff12b355d9feab009cf6033b6
ranjitweinvest <ranjithkumar@railsfactory.org>
2018-06-29 15:49:34 +0530

    added portfolio, portfolioDetails, Notfound components

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="container">
+<h1>{{title}}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Portfolio',
+  data () {
+    return {
+      title: 'Portfolio'
+    }
+  }
+}
+</script>
+<style>
+
+</style>
+
---
+++ src/components/PortfolioDetails.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="container">
+    <h1>{{title}}</h1>
+</div>    
+</template>
+
+<script>
+export default {
+ name: 'PortfolioDetails',
+  data () {
+    return {
+      title: 'PortfolioDetails'
+    }
+  }
+}
+</script>
+<style>
+
+</style>
