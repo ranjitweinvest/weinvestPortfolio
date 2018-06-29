92184f4c145ef702570d78bd6cef07cd421bee4d
ranjitweinvest <ranjithkumar@railsfactory.org>
2018-06-29 16:04:14 +0530

    router id added in portfoliodetails page

--- src/components/PortfolioDetails.vue
+++ src/components/PortfolioDetails.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container">
-    <h1>{{title}}</h1>
+    <h1>{{title}}{{id}}</h1>
 </div>    
 </template>
 
@@ -9,9 +9,13 @@ export default {
  name: 'PortfolioDetails',
   data () {
     return {
-      title: 'PortfolioDetails'
+      title: 'PortfolioDetails',
+      id: null,
     }
-  }
+  },
+  created() {
+    this.id = this.$route.params.id;
+  },
 }
 </script>
 <style>
