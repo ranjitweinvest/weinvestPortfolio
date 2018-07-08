abad853411650feffc7d27e341c6ab0cca3cc986
ranjitweinvest <ranjithkumar@railsfactory.org>
2018-07-08 23:14:27 +0530

    express static files serving added

--- server/index.js
+++ server/index.js
@@ -4,7 +4,12 @@ const PORT = process.env.PORT || 8080;
 const path = require('path');
 const app = express();
 app.use(bodyParser.json());
-app.use('/',express.static(`${__dirname}/../dist`));
+app.use(express.static(`${__dirname}/../dist`));
+
+app.use('/js', express.static(`${__dirname}/../dist/static/js/`));
+app.use('/css', express.static(`${__dirname}/../dist/static/css/`));
+app.use('/img', express.static(`${__dirname}/../dist/static/css/`));
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/../dist/index.html`));
 });
