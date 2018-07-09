36d01dd5d889b330d6bf4e966984c8d39d24c0bd
ranjitweinvest <ranjithkumar@railsfactory.org>
2018-07-09 09:38:26 +0530

    connect-history-api-fallback module add in server.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const history = require('connect-history-api-fallback');
 const PORT = process.env.PORT || 8080;
 const path = require('path');
 const app = express();
 app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../dist`));
+app.use(history());
 app.use(express.static(`${__dirname}/../dist`));
 
 app.use('/js', express.static(`${__dirname}/../dist/static/js/`));
 app.use('/css', express.static(`${__dirname}/../dist/static/css/`));
 app.use('/img', express.static(`${__dirname}/../dist/static/css/`));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/../dist/index.html`));
 });
 app.listen(PORT, () => {
